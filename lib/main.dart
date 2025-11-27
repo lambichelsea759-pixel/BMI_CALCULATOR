32652ef7b6d94b34135340eafd0956e388206952
favourchelsealambi <lambichelsea2@icloud.com>
2025-11-27 13:09:21 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(BMICalculatorApp());
+}
+
+class BMICalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: BMIScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BMIScreen extends StatefulWidget {
+  @override
+  _BMIScreenState createState() => _BMIScreenState();
+}
+
+class _BMIScreenState extends State<BMIScreen> {
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+  String _result = "";
+
+  void _calculateBMI() {
+    final double? height = double.tryParse(_heightController.text);
+    final double? weight = double.tryParse(_weightController.text);
+
+    if (height != null && weight != null && height > 0) {
+      final double bmi = weight / ((height / 100) * (height / 100));
+      String
